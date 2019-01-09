9f327c8e67318c1ce338ee43d7f53000bb502757
abdulrabbt <abdulrab.bt@hotmail.com>
2019-01-09 22:01:45 +0300

    done

---
+++ reading-app/src/components/ShowBook.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ShowBook = (props) => {
+    return (
+        <div className="book" onMouseOver={() => { props.setCurrentShow(props.book) }}>
+
+            <button onClick={() => { props.deleteBook(props.book.id) }}>X</button>
+            <h2>Title: {props.book.title}</h2>
+            <h2>Author: {props.book.author}</h2>
+            <h2>Release Date: {props.book.release_date}</h2>
+        </div>
+    )
+}
+export default ShowBook;
