acd62eecb00c6191ee0830eca4f4bb98d78d8683
YA7MO <ya7yamo1@gmail.com>
2019-01-09 19:34:52 +0300

    Done

---
+++ books-app/src/App.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import './App.css';
+import Books from './components/Books';
+import NewBook from './components/NewBook'
+class App extends Component {
+
+  constructor(){
+    super()
+
+    this.state = {
+      books:[],
+      modal: false,
+      image:''
+    }
+
+  }
+
+  componentDidMount(){
+    console.log('fetching data');
+    fetch('http://myapi-profstream.herokuapp.com/api/8c36bf/books')
+      .then( response => response.json())
+      .then( data => {
+        console.log(data);
+        this.setState({
+          books: data
+        })
+      })
+      .catch( error => {
+        console.log(error)
+      })
+  }
+
+  renderBooks(allBooks) {
+    return allBooks.map((book) => {
+      return (
+        <Books key={book.id}
+          book={book} showImage = {this.showImage.bind(this)}/>
+      )
+    })
+  }
+
+  showImage(newImage){
+    console.log('New image',newImage);
+    this.setState({
+      image: newImage
+    })
+ 
+  
+  }
+
+  renderImage(i){
+    return (
+      <img src={i} alt=""/>
+    )
+  }
+
+  createNewBook(show) {
+   
+   console.log('updating show');
+   const url = 'http://myapi-profstream.herokuapp.com/api/8c36bf/books'
+   fetch(url, {
+       method: 'POST',
+       headers: {
+         "Content-Type": "application/json"
+       },
+       body: JSON.stringify(show)
+     })
+     .then(response => response.json())
+     .then(data => {
+       console.log('DATA')
+       console.log(data);
+       const updatedBooks = this.state.books.concat([data]);
+       console.log(updatedBooks)
+       this.setState({
+         books: updatedBooks
+       })
+     })
+     .catch(error => {
+       console.log(error);
+     })
+  }
+
+
+  toggleModal(){
+    console.log('toggle modal');
+    this.setState({
+      activeShow: null,
+      modal: !this.state.modal
+    })
+  }
+  render() {
+    return (
+      <div className="app">
+      <div>
+              <button onClick={this.toggleModal.bind(this)}>Add A New Books</button>
+              {this.state.modal ? <NewBook createNewBook={this.createNewBook.bind(this)}/> : ''}
+      </div>
+      <div className="books">
+         {this.renderBooks(this.state.books)}
+
+      </div>
+      <div className = "image">
+          {this.renderImage(this.state.image)}
+      </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ books-app/src/components/NewBook.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class NewBook extends Component {
+  constructor(){
+    super();
+    this.state = {
+      title: '',
+      author: '',
+      release_date: '',
+      image: ''
+    }
+  }
+  handleChange(event){
+    const currentInput = event.target.name;
+    const newValue = event.target.value;
+    // console.log('current input: ', currentInput);
+    // console.log('newValue: ', newValue);
+
+    this.setState({
+      [currentInput]: newValue
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.createNewBook(this.state)
+  }
+  
+  render(){
+    return(
+      <div>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <label>title:</label><input type="text" name="title" onChange={this.handleChange.bind(this)}/><br/>
+          <label>author:</label><input type="text" name="author" onChange={this.handleChange.bind(this)}/><br/>
+          <label>release_date:</label><input type="text" name="release_date" onChange={this.handleChange.bind(this)}/><br/>
+          <label>image:</label><input type="text" name="image" onChange={this.handleChange.bind(this)}/><br/>
+          <button>submit</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default NewBook;
+
